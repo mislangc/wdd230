5f15946bc6d3c9ba2a2bb9874d2261241952d3f2
mislangc <carlbpmislang@gmail.com>
2023-05-15 19:09:57 -0700

    week 5 chamber continued Finished.

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -16,3 +16,14 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger")
 x.onclick = toggleMenu;
+
+//Monday and Tuesday banner
+
+const d = new Date();
+const day = d.getDay();
+
+if (day == 1 || day == 2) {
+    const banner = document.querySelector("#banner");
+    banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    banner.style.display = "block";
+}
